fe9888371d373ef6dba83da910c56922bf0e3937
Isaac Gómez <isaac@gomez.sv>
2013-04-21 21:49:09 -0600

    Refactor Asteroide class and enhance game controls; add Disparo class for projectile management

---
+++ Nave B/Disparo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Nave_B
+{
+    class Disparo
+    {
+        public SolidBrush ColorD { set; get; }
+        public int Y { set; get; }
+        public int X { set; get; }
+        public int Ancho { set; get; }
+        public int Alto { set; get; }
+
+        public Disparo(int x, int y, int ancho, int alto, Color color) {
+            this.X = x;
+            this.Y = y;
+            this.Ancho = ancho;
+            this.Alto = alto;
+            ColorD = new SolidBrush(color);
+        }
+
+        public GraphicsPath getPath() {
+            GraphicsPath disparo = new GraphicsPath();
+            disparo.AddRectangle(new Rectangle(this.X, this.Y, this.Ancho, this.Alto));
+            this.X += 10;
+            return disparo;
+        }
+    }
+}
--- Nave B/Juego.cs
+++ Nave B/Juego.cs
@@ -20,31 +20,39 @@ namespace Nave_B
         private Bitmap asteroide;
         private BufferedGraphicsContext buffercontext;
         private BufferedGraphics buffer;
+        private int ticks = 0, puntos = 0, prenivel = 0, nivel = 0;
 
         public Juego(BufferedGraphicsContext bcontext)
         {
             InitializeComponent();
-            this.buffercontext = bcontext;
-            nav = new Nave(20, Game.Height / 2);
-            Fondo = new Rectangle(0,0,Game.Width,Game.Height);
-            background = Properties.Resources.back;
-            naves = Properties.Resources.nave;
-            asteroide = Properties.Resources.asteroide;
-            back_time.Enabled = true;
-            ast = new Asteroide(Game.Width, Game.Height);
+            this.buffercontext = bcontext; // BufferContext desde el Frame
+            nav = new Nave(20, Game.Height / 2); // Posicion inicial de la nave
+            Fondo = new Rectangle(0,0,Game.Width,Game.Height); 
+            background = Properties.Resources.back; // imagen de Fondo
+            naves = Properties.Resources.nave; // sprites de la nave
+            asteroide = Properties.Resources.asteroide; // imagen de asteroide
+            back_time.Enabled = true; // timer (es como un while infinito que se encarga de realizar los cambios)
+            ast = new Asteroide(Game.Width, Game.Height); // Espacio que el asteroide tiene para moverse
+            control.Focus();
         }
 
         private void fondo(){
+            /* Dibuja el Fondo */
             buffer.Graphics.DrawImage(background, 0, 0, Fondo, GraphicsUnit.Pixel);
 
-            // Despues arreglo lo del fondo que se desfasa
+            //TODO: Despues arreglo lo del fondo que se desfasa
+            /* Verificamos si el fondo ha llegado a su fin y redibujamos la otra parte desde el principio */
             int division = (background.Width - Fondo.X);
             if (division < Game.Width)
             {
                 Fondo.Width = division;
                 buffer.Graphics.DrawImage(background, division, 0, new Rectangle(0, 0, Game.Width - division, Game.Height), GraphicsUnit.Pixel);
             }
-            Fondo.X += 5; // velociadad del fondo
+
+            /* Velocidad de desplazamiento del fondo
+             * Aumenta con el tiempo de juego
+             */
+            Fondo.X += (2 + prenivel);
             if (Fondo.X > background.Width)
             {
                 Fondo.X = 0;
@@ -54,53 +62,59 @@ namespace Nave_B
 
         private void back_time_Tick(object sender, EventArgs e)
         {
+            /* Dimesiones del buffer a utilizar. Ancho y alto del PictureBox */ 
             buffer = buffercontext.Allocate(Game.CreateGraphics(), Game.DisplayRectangle);
+            /* Dibuja el fondo */ 
             fondo();
+            /* Dibuja la nave */
+            nav.getPosision();
             buffer.Graphics.DrawImage(naves, nav.X, nav.Y, nav.getNave(), GraphicsUnit.Pixel);
+            for (int i = 0; i < nav.Disparos.Count; i++) {
+                buffer.Graphics.FillPath(nav.Disparos[i].ColorD, nav.Disparos[i].getPath());
+            }
+            /*Dibuja el asteroide */
             buffer.Graphics.TranslateTransform(ast.X, ast.Y);
             ast.Rotar();
             buffer.Graphics.DrawImage(asteroide, ast.getPuntos()); //dibuja el bitmap , especificando un arreglo de 3 puntos
             ast.DesplazarAsteroide();
             buffer.Graphics.TranslateTransform(-ast.X, -ast.Y);
-            // No colocar codigo para dibujar despues de aqui.
+
+            /* No colocar codigo para dibujar despues de aqui. */
+            Puntos();
             buffer.Render();
             buffer.Dispose();
         }
             
-        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
-        {
-            if (keyData == Keys.Left)
+        private void Puntos(){
+            ticks++;
+            if (ticks > 100)
             {
-                nav.moverIzquierda();
-                return true;
+                ticks = 0;
+                puntos+=10;
             }
-            else if (keyData == Keys.Right)
+            if (puntos != 0 && puntos%1000 == 0)
             {
-                nav.moverDerecha();
-                return true;
+                prenivel++;
             }
-            else if (keyData == Keys.Up)
+            if (prenivel > 100)
             {
-                nav.moverArriba();
-                return true;
+                prenivel = 0;
+                nivel++;
             }
-            else if (keyData == Keys.Down)
-            {
-                nav.moverAbajo();
-                return true;
-            }
-            else if (keyData == Keys.X)
-            {
-                nav.disparar();
-                return true;
-            }
-            else
-                return base.ProcessCmdKey(ref msg, keyData);
+            lpuntos.Text = "Puntos: " + puntos;
         }
 
-        
-        
-        
+        private void control_KeyDown(object sender, KeyEventArgs e)
+        {
+            nav.mover(e.KeyCode);
+            control.Text = "";
+        }
+
+        private void control_KeyUp(object sender, KeyEventArgs e)
+        {
+            nav.detener(e.KeyCode);
+            control.Text = "";
+        }
 
     }
 }
